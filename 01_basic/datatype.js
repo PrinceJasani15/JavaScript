64af40024b1a1e0a1a7733e40a581d566d49cdd2
Prince <princejasani15@gmail.com>
2026-06-08 07:53:15 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+let name = "prince";
+let age = 23;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint => a vadhare mota number mate use thay che and moti company jemke facebook, stockmarket etc
+// string => "" or ''
+// boolean => true or false
+// null => empty value
+// undefined => value is not assigned
+// symbol => unique finder a react use thay che and ky k unique value generate karva mate use thay che
+// object => array, function, object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
