454bd68627212854854a658b3857db7b4ffcf06a
Prince <princejasani15@gmail.com>
2026-06-08 07:29:54 +0000

    variable chages done

---
+++ 01_basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234567890;
+let accountEmail = "prince@gmail.com";
+var accountpassword = "1234";
+accountCity = "surat";
+
+
+accountEmail = "pj@gmail.com";
+accountPassword = "3456";
+accountCity = "Ahmedabad";
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountpassword, accountCity, accountEmail, accountPassword]);
